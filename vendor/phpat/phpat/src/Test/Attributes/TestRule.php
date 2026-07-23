fa1c078ac87aef4c1c763e8eaa7db78075f7def3
SupplyChainNexus <info@supplychainnexus.co.za>
2026-07-23 23:01:15 +0200

    chore: remove vendor directory from git tracking and add .gitignore

--- vendor/phpat/phpat/src/Test/Attributes/TestRule.php
+++
@@ -1,6 +0,0 @@
-<?php declare(strict_types=1);
-
-namespace PHPat\Test\Attributes;
-
-#[\Attribute(\Attribute::TARGET_METHOD)]
-final class TestRule {}
--- vendor/phpat/phpat/src/Test/Builder/BuildStep.php
+++
@@ -1,5 +0,0 @@
-<?php declare(strict_types=1);
-
-namespace PHPat\Test\Builder;
-
-class BuildStep extends AbstractStep {}
